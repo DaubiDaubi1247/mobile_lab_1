6cf8e557a91bc9787be50e8cc75bc64fd97132cd
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-02-27 11:30:57 +0300

    add button in MainActivity, and create DetailActivity

--- app/src/main/java/ru/alex/lab1/MainActivity.java
+++ app/src/main/java/ru/alex/lab1/MainActivity.java
@@ -2,7 +2,11 @@ package ru.alex.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import ru.alex.lab1.activity.DetailActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button navBtn = findViewById(R.id.navBtn);
+
+        navBtn.setOnClickListener(e -> {
+            Intent refToDetail = new Intent(this, DetailActivity.class);
+            startActivity(refToDetail);
+        });
     }
 }
--- app/src/main/java/ru/alex/lab1/activity/DetailActivity.java
+++ app/src/main/java/ru/alex/lab1/activity/DetailActivity.java
@@ -1,4 +1,16 @@
 package ru.alex.lab1.activity;
 
-public class DetailActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.alex.lab1.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
 }
