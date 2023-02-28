e826d5507bd098e948883e990c572ea7dfc4d085
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-02-28 18:36:41 +0300

    create github button and change backround button

--- app/src/main/java/ru/alex/lab1/MainActivity.java
+++ app/src/main/java/ru/alex/lab1/MainActivity.java
@@ -1,8 +1,10 @@
 package ru.alex.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -16,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button navBtn = findViewById(R.id.navBtn);
+        Button navToGithub = findViewById(R.id.navToGithub);
+
+        navToGithub.setOnClickListener(e -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://github.com/JustAlex322"));
+            startActivity(intent);
+        });
 
         navBtn.setOnClickListener(e -> {
             Intent refToDetail = new Intent(this, DetailActivity.class);
