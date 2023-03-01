749d7098fc1cb60c16be3b850e4c134e83b92aea
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-03-01 13:34:14 +0300

    add lifecycles methods

--- app/src/main/java/ru/alex/lab1/MainActivity.java
+++ app/src/main/java/ru/alex/lab1/MainActivity.java
@@ -6,11 +6,49 @@ import androidx.appcompat.app.AppCompatDelegate;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import ru.alex.lab1.activity.DetailActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w("MainActivity", "on start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "on resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("MainActivity", "on pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w("MainActivity", "on stop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.w("MainActivity", "on restart");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("MainActivity", "on destroy");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/ru/alex/lab1/activity/DetailActivity.java
+++ app/src/main/java/ru/alex/lab1/activity/DetailActivity.java
@@ -3,10 +3,49 @@ package ru.alex.lab1.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import ru.alex.lab1.R;
 
 public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w("DetailActivity", "on start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("DetailActivity", "on resume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("DetailActivity", "on pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w("DetailActivity", "on stop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.w("DetailActivity", "on restart");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("DetailActivity", "on destroy");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
