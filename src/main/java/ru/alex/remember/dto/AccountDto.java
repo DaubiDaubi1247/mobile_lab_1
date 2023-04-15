d89b49173c5922bc6b3a0b3054a1bd65cf3ff502
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-04-15 20:45:41 +0300

    create AccountDto and add jakarta validation

---
+++ src/main/java/ru/alex/remember/dto/AccountDto.java
@@ -0,0 +1,23 @@
+package ru.alex.remember.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@SuperBuilder
+public class AccountDto {
+
+    @NotBlank(message = "name must be not null and not empty")
+    private String name;
+
+    @NotBlank(message = "role must be not null and not empty")
+    private String role;
+
+    @NotBlank(message = "password must be not null and not empty")
+    private String password;
+}
