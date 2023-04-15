984bad0097a7380b81f115ab4906fab97ab4a828
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-04-15 20:47:38 +0300

    create AccountWithPasswordDto

--- src/main/java/ru/alex/remember/dto/AccountWithPasswordDto.java
+++ src/main/java/ru/alex/remember/dto/AccountWithPasswordDto.java
@@ -1,7 +1,15 @@
 package ru.alex.remember.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@SuperBuilder
 public class AccountWithPasswordDto extends AccountDto{
 
     @NotBlank(message = "password must be not null and not empty")
